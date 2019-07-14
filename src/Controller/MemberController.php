43bb51ed093924f1fc7e5d64a771631b64e8ab61
Sir Kane <ahmadouwalyndiaye@gmail.com>
2019-07-14 14:08:57 +0000

    Integrated easy admin

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/")
+ * @Route("/member")
  */
 class MemberController extends AbstractController
 {
