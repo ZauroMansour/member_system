a827d92b6a2e0de7d795dc7052832b66deab26da
ZauroMansour <53145598+ZauroMansour@users.noreply.github.com>
2019-07-21 21:03:35 +0000

    Add files via upload

--- src/Entity/Member.php
+++ src/Entity/Member.php
@@ -3,10 +3,20 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MemberRepository")
  */
+
+/**
+ * @ORM\Entity
+ * @Vich\Uploadable
+ */
+
 class Member
 {
     /**
@@ -27,12 +37,60 @@ class Member
     private $lastname;
 
     /**
-     * @ORM\Column(type="string", length=20, nullable=true)
+     * @ORM\Column(type="date", nullable=false)
+     */
+    private $datenaiss;
+
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string", length=10)
+     */
+    private $genre;
+
+    /**
+     * @Vich\UploadableField(mapping="member_image", fileNameProperty="imageName")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="situation_familiale", type="string", length=50)
+     */
+    private $situationFamiliale;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=false)
      */
     private $phone;
 
     /**
-     * @ORM\Column(type="date", nullable=true)
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $autrephone;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    private $structure;
+
+
+    /**
+     * @ORM\Column(type="date", nullable=false)
      */
     private $anneeAdhesion;
 
@@ -40,6 +98,11 @@ class Member
      * @ORM\Column(type="string", length=255)
      */
     private $address;
+    
+    public function __construct()
+    {
+        $this->image = new EmbeddedFile();
+    }
 
     public function getId(): ?int
     {
@@ -102,6 +165,118 @@ class Member
     public function setAddress(string $address): self
     {
         $this->address = $address;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getSituationFamiliale(): ?string
+    {
+        return $this->situationFamiliale;
+    }
+
+    public function setSituationFamiliale(string $situationFamiliale): self
+    {
+        $this->situationFamiliale = $situationFamiliale;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile $imageFile
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+    
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+public function setUpdatedAt(\DateTime $datetime)
+{
+    $this->updatedAt = $datetime;
+
+    return $this;
+}
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+    
+    public function setImageName(?string $image)
+    {
+        $this->imageName = $image;
+    
+        return $this;
+    }
+
+    public function getFullName()
+    {
+        return $this->getFirstName().' '.$this->getLastName();
+    }
+
+    public function getDatenaiss(): ?\DateTimeInterface
+    {
+        return $this->datenaiss;
+    }
+
+    public function setDatenaiss(?\DateTimeInterface $datenaiss): self
+    {
+        $this->datenaiss = $datenaiss;
+
+        return $this;
+    }
+
+    public function getStructure(): ?string
+    {
+        return $this->structure;
+    }
+
+    public function setStructure(string $structure): self
+    {
+        $this->structure = $structure;
+
+        return $this;
+    }
+
+    public function getAutrephone(): ?string
+    {
+        return $this->autrephone;
+    }
+
+    public function setAutrephone(?string $autrephone): self
+    {
+        $this->autrephone = $autrephone;
 
         return $this;
     }
--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -6,6 +6,15 @@ use App\Entity\Member;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class MemberType extends AbstractType
 {
@@ -14,9 +23,28 @@ class MemberType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('phone')
-            ->add('anneeAdhesion')
-            ->add('address')
+            ->add('datenaiss', DateType::class, array(
+                'label' => "Date de naissance",
+                'widget' => 'single_text',
+                'html5' => true,
+                'required' => true,
+                'attr' => ['class' => 'datepicker'],))
+            ->add('genre', ChoiceType::class, array('choices' => array('Masculin' => 'Masculin', 'Féminin' => 'Féminin')))
+            ->add('situationFamiliale', ChoiceType::class, array('choices' => array('Célibataire' => 'Celibataire', 'Marié(e)' => 'Marié', 'Divorcé(e)' => 'Divorcé', 'Veuf(ve)' => 'Veuf')))
+            ->add('structure', TextType::class)
+            ->add('phone', TextType::class, ['required'  =>  true])
+            ->add('autrephone', TextType::class, ['required'  =>  false])
+            ->add('anneeAdhesion', DateType::class, array(
+                'label' => "Date d'adhésion",
+                'widget' => 'single_text',
+                'html5' => true,
+                'required' => true,
+                'attr' => ['class' => 'datepicker'],
+                ))
+            ->add('address', TextareaType::class)
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => true])
         ;
     }
 
